32eb9322b6f36e736f5428e5882769d92f86bae2
DarkDevil99 <87717476+DarkDevil99@users.noreply.github.com>
2021-10-16 17:35:01 +0530

    Create Number Range.py

---
+++ Number Range.py
@@ -0,0 +1,14 @@
+# Python program to print positive Numbers in a List
+
+# list of numbers
+
+list1 = [12, -22, 0, 45, 88, -93]
+
+# iterating each number in list
+
+for num in list1:
+	
+	# checking condition
+	
+  if num >= 0:
+	    print(num, end = " ")
